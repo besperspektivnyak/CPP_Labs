f415b11f0557b2d4ec2de6db63c31e17b111fed6
besperspektivnyak <48994521+besperspektivnyak@users.noreply.github.com>
2020-03-25 13:35:05 +0300

    Add files via upload

---
+++ First_Lab/functions.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <sstream>
+#include <cstdlib>
+
+using namespace std;
+
+void LessLen(vector<string> words, int length, int len);
+void Equal(vector<string> words);
+void TextToCenter(string str, int length);
