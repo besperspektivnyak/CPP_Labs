5a7789219737b25429d2d7ae46e2e6c139b0bf2f
Elizaveta Grishina <48994521+besperspektivnyak@users.noreply.github.com>
2020-06-10 21:53:27 +0300

    Add files via upload

---
+++ Lab5/Menu.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <GL\glut.h>
+
+class Position
+{
+private:
+	double x_;
+	double y_;
+public:
+	Position() { x_ = 0.0, y_ = 0.0; }
+	Position(double x, double y) { x_ = x; y_ = y; }
+	void RasterPos2f(void) { glRasterPos2f(x_, y_); }
+};
+
+class MenuElem
+{
+private:
+	std::vector<MenuElem> subMenu_;
+	MenuElem* parent_;
+	std::string title_;
+	Position pos_;
+public:
+	MenuElem( MenuElem* parent, std::string elem, Position pos);
+	MenuElem();
+	std::vector<MenuElem> getSubMenu() { return subMenu_; }
+	MenuElem getSubMenuElem(unsigned index) { return getSubMenu()[index]; }
+	MenuElem* getParent() { return parent_; }
+	std::string getTitle() { return title_; }
+	void print();
+	void addSubMenu(MenuElem elem) { subMenu_.push_back(elem); }
+	void setPar(MenuElem *parent) { parent_ = parent; }
+	void setTitle(std::string elem) { title_ = elem; }
+	void setPos(Position pos) { pos_ = pos; }
+	MenuElem operator=(const MenuElem& right)
+	{
+		title_ = right.title_;
+		parent_ = right.parent_;
+		pos_ = right.pos_;
+		subMenu_ = right.subMenu_;
+		return *this;
+	}
+};
+
+class Menu
+{
+private:
+	std::vector<MenuElem> elems_;
+public:
+	std::vector<MenuElem>& getElems() { return elems_; }
+	void addElem(MenuElem newElem) { elems_.push_back(newElem); }
+	MenuElem getElement(unsigned index) { return getElems()[index]; }
+	void print();
+};
---
+++ Lab5/main.cpp
@@ -0,0 +1,71 @@
+﻿#include "Menu.h"
+#include "ReadMenu.h"
+#include "paint.h"
+#include <Gl\glut.h>
+#include <GL\GLU.h>
+
+Menu menu1;
+
+int windW = 1100;
+int windH = 600;
+
+void changeSize(int w, int h)
+{
+	if (h == 0)
+		h = 1;
+	float ratio = w * 1.0 / h;
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0, w, 0, h);
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_MODELVIEW);
+	windW = w;
+	windH = h;
+}
+
+void renderScene(void)
+{
+  glClearColor(1.0, 1.0, 1.0, 1.0);
+	glColor3f(1.0, 0.0, 0.0);
+	menu1.print();
+	DrawColorPalette();
+	DrawSizes();
+	glutSwapBuffers();
+}
+
+void processNormalKeys(unsigned char key, int x, int y)
+{
+	if (key == 27)
+		exit(0);
+}
+
+void Timer(int)
+{
+	renderScene();
+	glutTimerFunc(40, Timer, 0);
+}
+
+int main(int argc, char** argv)
+{
+	menu1 = CreateMenu("test.txt");
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(windW, windH);
+	glutCreateWindow("Paint");
+
+	glutKeyboardFunc(processNormalKeys);
+
+	glutDisplayFunc(renderScene);
+	glutReshapeFunc(changeSize);
+	glutTimerFunc(40, Timer, 0);
+
+	glutMotionFunc(mouseMove);
+	glutMouseFunc(mouseClick);
+
+	glutMainLoop();
+
+	return 1;
+}
+
